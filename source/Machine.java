241cc24f4d0e5c90c38d30e286fd81b63b1fb2c3
anologicon <pauloraimundi2009@hotmail.com>
2017-04-11 19:51:31 -0300

    New Engine for Machine

--- source/Machine.java
+++ source/Machine.java
@@ -1,19 +1,24 @@
 public class Machine {
 
   private String number_ip;
+  private String a[];
   public Machine()
   {
     this.number_ip = number_ip;
+    this.a = new String[10];
   }
 
 
-  public int convert(String ip){
-    String aux[] = ip.split(".");
-
-    int parte1 = String.valueOf(aux[0],2);
-
-    this.number_ip = parte1;
-
+  public String parse(String ip){
+    String[] aux = ip.split("\\.");
+    a = Integer.parseInt(aux[0]);
+    for (i = 1; i <= 10; i++) {
+        echo $i;
+    }
+    
+    String binario = Integer.toBinaryString(a);
+  
+    
   }
 
 }
